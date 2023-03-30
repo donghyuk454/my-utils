7b8c1642ba03382f88ee3a0cdb0bc92a52a7b06d
dongdong <donghyuk454@gmail.com>
2023-03-30 20:35:34 +0900

    Refactor: 코드 refactoring

--- src/main/java/com/dong/transformer/MapTransformer.java
+++ src/main/java/com/dong/transformer/MapTransformer.java
@@ -3,17 +3,15 @@ package main.java.com.dong.transformer;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapTransformer implements Transformer {
 
-    public static <T> T transformTo (Map map, Class<T> className) throws Exception{
+    public <T> T transformTo (Map<String, String> map, Class<T> className) throws Exception {
         if (map == null)
             return null;
-        Set<Object> keySet = map.keySet();
-        Iterator<Object> iterator = keySet.iterator();
+        Set<String> keySet = map.keySet();
+        Iterator<String> iterator = keySet.iterator();
         T obj = null;
         try {
             obj = className.getConstructor().newInstance(); // 빈 객체 생성
@@ -23,26 +21,18 @@ public class MapTransformer implements Transformer {
 
         // < key, value > 연산
         while (iterator.hasNext()) {
-            Object key = iterator.next();
-            Object value = map.get(key);
-            String setterName = new StringBuilder()
-                    .append("set")
-                    .append(key.toString().substring(0,1).toUpperCase())
-                    .append(key.toString().substring(1))
-                    .toString();
-            Field field = className.getDeclaredField(key.toString()); // key 와 같은 field 찾기
+            String key = iterator.next();
+            String value = map.get(key);
+            String setterName = "set" +
+                    key.substring(0, 1).toUpperCase() +
+                    key.substring(1);
+            Field field = className.getDeclaredField(key); // key 와 같은 field 찾기
             Class<?> fieldType = field.getType();
             try {
                 Method setter = obj.getClass().getMethod(setterName, fieldType);
                 if (setter.getParameterCount() != 1) {
                     throw new Exception("not invalid setter");
                 }
-                System.out.println("setter = " + setter);
-                System.out.println("field = " + field);
-                String fieldTypeName = fieldType.getTypeName();
-                System.out.println("type = " + fieldTypeName);
-                System.out.println(Integer.class.getTypeName());
-
                 invokeSetter(obj, setter, fieldType, value);
 
             } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
